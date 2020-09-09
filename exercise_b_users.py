dc5623b082d0801884d9ac7a61f5193fdc8cec93
fatpav <fat_pav@hotmail.com>
2020-09-09 15:12:51 +0100

    created a loop for Avril's lottery numbers

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -61,19 +61,31 @@ print(users["Jonathan"]["twitter"])
 # 2. Get Erik's hometown
 
 print(users["Erik"]["home_town"])
+
 # 3. Get the array of Erik's lottery numbers
+
 print(users["Erik"]["lottery_numbers"])
 
 # 4. Get the species of Avril's pet Monty
 
 print(users["Avril"]["pets"][0]["species"])
-#Go to pets and access list to access dictionary within
+
+#Go to pets and access list to access dictionary within LOOK FOR LIST BRACKETS
 
 # 5. Get the smallest of Erik's lottery numbers
 
 print(min(users["Erik"]["lottery_numbers"]))
 
 # 6. Return an array of Avril's lottery numbers that are even
+
+avrils_numbers = users["Avril"]["lottery_numbers"]
+even_numbers = []
+for number in avrils_numbers:
+  if number % 2 == 0:
+    even_numbers.append(number)
+
+print(even_numbers)
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
