3e9912a9ce20badfed64d22963167023a8f16b1b
fatpav <fat_pav@hotmail.com>
2020-09-09 14:15:59 +0100

    Did steps 6 and 7

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -25,7 +25,14 @@ stops.remove("Livingston")
 print(stops)
 
 #6. Delete "Cumbernauld" from the list by index
+
+stops.pop(2)
+print(stops)
+
 #7. Print the number of stops there are in the list
+
+print(len(stops))
+
 #8. Sort the list alphabetically
 #9. Reverse the positions of the stops in the list
 #10 Print out all the stops using a for loop
